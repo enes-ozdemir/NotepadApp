2bfd5ffdbd93fa895c912609142b3717271c3614
Enes Özdemir <enesozdemir6167@hotmail.com>
2020-05-31 21:30:40 +0300

    Initial Commit

--- app/src/main/java/com/eozdemir/notepad/MainActivity.java
+++ app/src/main/java/com/eozdemir/notepad/MainActivity.java
@@ -1,14 +1,71 @@
 package com.eozdemir.notepad;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+    RecyclerView mRecyclerView;
+    FloatingActionButton mAddNote;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.rvmain);
+        mAddNote = findViewById(R.id.addNote);
+        setToolbar();
+
+        mAddNote.setRippleColor((getResources().getColor(R.color.design_default_color_primary_dark)));
+
+        mAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+    private void setToolbar() {
+
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setTitleTextColor(getResources().getColor(R.color.textPrimaryColor));
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
